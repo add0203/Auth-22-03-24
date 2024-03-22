8f103954349eb17f93aec9c62926745f04c94bd7
anand dhar dwivedi <ananddhardwivedi05@gmail.com>
2024-03-22 14:42:04 +0530

    22-03-2024

---
+++ login.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const loginForm = document.getElementById("loginForm");
+  const errorMessage = document.getElementById("errorMessage");
+
+  loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const email = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    // Perform client-side validation if needed
+
+    fetch("http://localhost:3002/user/sign-in", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Login failed");
+        }
+        window.location.href = "/client/dashboard.html"; // Redirect to dashboard page after successful login
+      })
+      .catch((error) => {
+        errorMessage.textContent = error.message;
+      });
+  });
+});
---
+++ signup.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const signupForm = document.getElementById("signupForm");
+  const errorMessage = document.getElementById("errorMessage");
+
+  signupForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const email = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    // Perform client-side validation if needed
+
+    fetch("http://localhost:3002/user/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Signup failed");
+        }
+        console.log("loged in");
+        window.location.href = "/client/login.html"; // Redirect to login page after successful signup
+      })
+      .catch((error) => {
+        errorMessage.textContent = error.message;
+      });
+  });
+});
